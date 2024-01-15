4ada3171d46c8bc9e7bfbdd173bc761ec386d450
kala0903 <kalandarishvililuka9@gmail.com>
2024-01-15 17:19:33 +0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-let
+
+let num = 50;
+
+if ( num< 25) {
+    console.log("ნაკლებია 50-ზე");
+} 
+    else if ( num > 10) {
+        console.log("მეტია 20-ზე");
+    }
+else {
+    console.log("Error");
+}
+
+let number1=10;
+let number2=15;
+let sum = number1 + number2;
+console.log(sum);
+
+
+
+
+let number =50 ;
+
+
+let result = number >= 15 ? "ture" : "false";
+
+console.log(result);
+
+
+
+    
+
+let favoriteanimal = "shark"
+
+switch (favoriteanimal){
+    case "dog":
+        console.log("dogs are kind");
+     break
+        case "cat":
+        console.log("cats are great");
+     break
+        case "shark":
+            console.log("interesting choice");
+     break
+}
+
+
+
