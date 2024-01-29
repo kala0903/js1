ca94a8e6f416349b4c9e1eba8945074aec398af2
kala0903 <kalandarishvililuka9@gmail.com>
2024-01-29 17:09:04 +0400

    script.js

--- script.js
+++ script.js
@@ -1,67 +1,82 @@
 'use strict';
 //1
-const numbers = [10, 50, 6, 7, 8, 11, 6, 3, 9]
-let sum1 = 0;
-numbers.forEach(numb => sum1 +=numb);
-console.log(sum1);
+let array2 = [14, 150, 'css', null, 'javascript', 25];
 
-//2
-let user = {
-    firstname: 'giorgi',
-    lastname: 'saakadze',
-    age: 32,
-    isloggedin: true
-  };
-function formatUserName(user) {
-    if (user.isloggedin) {
-      
-      return `${user.firstname} ${user.lastname}`;
-    } else {
-      
-      return false;
-    }
-  }
-
-  let result = formatUserName(user);
-  
-  if (result) {
-    console.log(`Formatted Name: ${result}`);
-  } else {
-    console.log('user is not logged in.');
-  }
-//3
-const numbers1 = [1,4,5,6,20,45]
-
-const largestnumber = (values) => {
-    let  highest = 0;
-    for (let i=0; i<values.length; i+=1){
-        if (values[i] > highest){
-            highest = values[i];
-        }
-
-    }
-    return highest;
+let newarray = array2.map((item) => {
+if (typeof item === 'number') {
+  return item * item;
+} else if (typeof item === 'string') {
+   return item.toUpperCase();
 }
-console.log(largestnumber(numbers1));
-//5
-let numbers2 = [1,2,3,4,5]; 
-const newarr = numbers2.concat().reverse();
-console.log(numbers2);
-console.log(newarr);
 
+})
 
-//4
-let number = 5 
-let reaminder = (number % 2)
-if (reaminder == 0){
-    console.log('{number} is even');
-} else{
-    console.log('{number} is odd');
-}
-//6
- const adult = (age) => {
-    return age >=19 ? 'სრულწლოვანი':'არასრულწლოვანი';
- }
- const adult1 = (60);
- console.log(adult1);
+ console.log(newarray);
+ //2
+ let info = 'good day';
+ const word = info.slice(5);
+ console.log(word);
+ //3
+ let strlength = (str) => str.length;
+ const er = strlength('hellooo');
+ console.log(er);
+ //4
  
+ // let userResponse = prompt("Capital of Georgia?");
+ // let userLowerCase = userResponse.toLowerCase();
+ // let correctAnswer = 'tbilisi';
+
+ // if (userLowerCase === correctAnswer) {
+    // console.log("Correct!");
+// } else {
+    //console.log("False");
+//}
+
+//5
+let fruits = ['apple', 'mango', 'avocado','kiwi']
+ let newfruit = fruits.splice(2,1, 'strawberry');
+ console.log(fruits);
+ //6
+  const numbers = [5, 25, 89, 120, 36];
+ numbers.push('javascript', 'python');
+ numbers.splice( 0,0, 'html', 'css');
+ console.log(numbers.length);
+ let newarray3 = numbers.slice(1,8);
+ console.log(newarray3);
+//7
+const fruits3 = ['orange', 'banana', 'kiwi'];
+console.log(fruits3.length, fruits3);
+fruits3.push('apple', 'pineapple');
+fruits3.splice(0,0, 'waterlemon');
+console.log(fruits3.length, fruits3);
+fruits3.splice(2,0, 'mango')
+console.log(fruits3.length,fruits3);
+//8
+const array4 = [1,2,3,4,5];
+let eu = array4.splice(3,0,'a','b','c');
+console.log(array4);
+//10
+const array6 = [15,100,25,10,36];
+const element = array6.splice (3,1);
+console.log(array6);
+//11
+const array7 = [1,2,3,4,5];
+const words = array7.splice(2,1, 'three');
+console.log(array7);
+//12
+let array8 =['hello1', 14,24,'hello2'];
+const newarr = array8.filter((elemnt) => typeof elemnt === 'numbers');
+console.log(array8);
+//13
+let array9 = [1,2,3,4,5];
+let sumarray9 = 0;
+let sum = array9.forEach((item => sumarray9 += item));
+console.log(sumarray9);
+//14
+const lang = ['html', 'css', 'javascript', 'python', 'php'];
+let filter = lang.filter((item => item.length > 3));
+console.log(filter);
+//15
+let worrds = ['madrid', 'rome', 'milan', 'berlin'];
+ let m = worrds.filter((item => item.includes('m')));
+ console.log(m);
